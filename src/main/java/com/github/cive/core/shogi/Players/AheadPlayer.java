aa27b6726e97a73b6a76981cf45a245058ae5c03
cive <cive@tk2-408-45178.vs.sakura.ne.jp>
2018-03-18 14:13:16 +0900

    [fix] can compile from mvn

--- src/main/java/com/github/cive/core/shogi/Players/AheadPlayer.java
+++ src/main/java/com/github/cive/core/shogi/Players/AheadPlayer.java
@@ -21,38 +21,38 @@ public class AheadPlayer extends PlayerBase {
     @Override
     protected void setFu() {
         for (int x = 1; x < 10; x++)
-            this.addPiecesOnBoard(factory.create(PieceBase.FU, new Point(x, 7)));
+            this.addPiecesOnBoard(factory.create(PieceBase.FU, new Point(x, 7)).get());
     }
     @Override
     protected void setKaku() {
-        this.addPiecesOnBoard(factory.create(PieceBase.KAKU, new Point(2,8)));
+        this.addPiecesOnBoard(factory.create(PieceBase.KAKU, new Point(2,8)).get());
     }
     @Override
     protected void setHisha() {
-        this.addPiecesOnBoard(factory.create(PieceBase.HISHA, new Point(8, 8)));
+        this.addPiecesOnBoard(factory.create(PieceBase.HISHA, new Point(8, 8)).get());
     }
     @Override
     protected void setKin() {
-        this.addPiecesOnBoard(factory.create(PieceBase.KIN, new Point(4, 9)));
-        this.addPiecesOnBoard(factory.create(PieceBase.KIN, new Point(6, 9)));
+        this.addPiecesOnBoard(factory.create(PieceBase.KIN, new Point(4, 9)).get());
+        this.addPiecesOnBoard(factory.create(PieceBase.KIN, new Point(6, 9)).get());
     }
     @Override
     protected void setGin() {
-        this.addPiecesOnBoard(factory.create(PieceBase.GIN, new Point(3, 9)));
-        this.addPiecesOnBoard(factory.create(PieceBase.GIN, new Point(7, 9)));
+        this.addPiecesOnBoard(factory.create(PieceBase.GIN, new Point(3, 9)).get());
+        this.addPiecesOnBoard(factory.create(PieceBase.GIN, new Point(7, 9)).get());
     }
     @Override
     protected void setKeima() {
-        this.addPiecesOnBoard(factory.create(PieceBase.KEIMA, new Point(2, 9)));
-        this.addPiecesOnBoard(factory.create(PieceBase.KEIMA, new Point(8, 9)));
+        this.addPiecesOnBoard(factory.create(PieceBase.KEIMA, new Point(2, 9)).get());
+        this.addPiecesOnBoard(factory.create(PieceBase.KEIMA, new Point(8, 9)).get());
     }
     @Override
     protected void setKyosha() {
-        this.addPiecesOnBoard(factory.create(PieceBase.KYOSHA, new Point(1, 9)));
-        this.addPiecesOnBoard(factory.create(PieceBase.KYOSHA, new Point(1, 9)));
+        this.addPiecesOnBoard(factory.create(PieceBase.KYOSHA, new Point(1, 9)).get());
+        this.addPiecesOnBoard(factory.create(PieceBase.KYOSHA, new Point(1, 9)).get());
     }
     @Override
     protected void setGyoku() {
-        this.addPiecesOnBoard(factory.create(PieceBase.GYOKU, new Point(5, 9)));
+        this.addPiecesOnBoard(factory.create(PieceBase.GYOKU, new Point(5, 9)).get());
     }
 }
--- src/main/java/com/github/cive/core/shogi/Players/BehindPlayer.java
+++ src/main/java/com/github/cive/core/shogi/Players/BehindPlayer.java
@@ -19,38 +19,38 @@ public class BehindPlayer extends PlayerBase {
     @Override
     protected void setFu() {
         for (int x = 1; x < 10; x++)
-            this.addPiecesOnBoard(factory.create(PieceBase.FU, new Point(x, 3)));
+            this.addPiecesOnBoard(factory.create(PieceBase.FU, new Point(x, 3)).get());
     }
     @Override
     protected void setKaku() {
-        this.addPiecesOnBoard(factory.create(PieceBase.KAKU, new Point(8, 2)));
+        this.addPiecesOnBoard(factory.create(PieceBase.KAKU, new Point(8, 2)).get());
     }
     @Override
     protected void setHisha() {
-        this.addPiecesOnBoard(factory.create(PieceBase.HISHA, new Point(2, 2)));
+        this.addPiecesOnBoard(factory.create(PieceBase.HISHA, new Point(2, 2)).get());
     }
     @Override
     protected void setKin() {
-        this.addPiecesOnBoard(factory.create(PieceBase.KIN, new Point(4, 1)));
-        this.addPiecesOnBoard(factory.create(PieceBase.KIN, new Point(6, 1)));
+        this.addPiecesOnBoard(factory.create(PieceBase.KIN, new Point(4, 1)).get());
+        this.addPiecesOnBoard(factory.create(PieceBase.KIN, new Point(6, 1)).get());
     }
     @Override
     protected void setGin() {
-        this.addPiecesOnBoard(factory.create(PieceBase.GIN, new Point(3, 1)));
-        this.addPiecesOnBoard(factory.create(PieceBase.GIN, new Point(7, 1)));
+        this.addPiecesOnBoard(factory.create(PieceBase.GIN, new Point(3, 1)).get());
+        this.addPiecesOnBoard(factory.create(PieceBase.GIN, new Point(7, 1)).get());
     }
     @Override
     protected void setKeima() {
-        this.addPiecesOnBoard(factory.create(PieceBase.KEIMA, new Point(2, 1)));
-        this.addPiecesOnBoard(factory.create(PieceBase.KEIMA, new Point(8, 1)));
+        this.addPiecesOnBoard(factory.create(PieceBase.KEIMA, new Point(2, 1)).get());
+        this.addPiecesOnBoard(factory.create(PieceBase.KEIMA, new Point(8, 1)).get());
     }
     @Override
     protected void setKyosha() {
-        this.addPiecesOnBoard(factory.create(PieceBase.KYOSHA, new Point(1, 1)));
-        this.addPiecesOnBoard(factory.create(PieceBase.KYOSHA, new Point(9, 1)));
+        this.addPiecesOnBoard(factory.create(PieceBase.KYOSHA, new Point(1, 1)).get());
+        this.addPiecesOnBoard(factory.create(PieceBase.KYOSHA, new Point(9, 1)).get());
     }
     @Override
     protected void setGyoku() {
-        this.addPiecesOnBoard(factory.create(PieceBase.GYOKU, new Point(5, 1)));
+        this.addPiecesOnBoard(factory.create(PieceBase.GYOKU, new Point(5, 1)).get());
     }
 }
